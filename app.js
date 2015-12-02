299940c4649208b93e187bc4b3cb9ff7e4671f27
emiliooo <emilburaczynski@gmail.com>
2015-12-02 18:24:56 +0100

    js srole done!

--- app.js
+++ app.js
@@ -9,9 +9,33 @@ $(document).ready(function() {
     });
     $(".topTwo").click(function(){
 
-        $('html, body').animate({scrollTop: $('.ab').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.our1').offset().top}, 'slow');
 
     });
+
+    $(".toptree").click(function(){
+
+        $('html, body').animate({scrollTop: $('.numb').offset().top}, 'slow');
+
+    });
+
+    $(".topfiv").click(function(){
+
+        $('html, body').animate({scrollTop: $('.portf').offset().top}, 'slow');
+
+    });
+
+    $(".topsix").click(function(){
+
+        $('html, body').animate({scrollTop: $('.form').offset().top}, 'slow');
+
+    });
+    $(".topse").click(function(){
+
+        $('html, body').animate({scrollTop: $('.form').offset().top}, 'slow');
+
+    });
+
 
 
 });
