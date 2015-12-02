951ac17cd4fd4745f5a97a827ea3576f27017835
emiliooo <emilburaczynski@gmail.com>
2015-12-02 14:46:52 +0100

    scroll new

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+$(document).ready(function() {
+    $(".topOne").click(function(){
+
+        $('html, body').animate({scrollTop: $('.ab').offset().top}, 'slow');
+
+    });
+    $(".topTwo").click(function(){
+
+        $('html, body').animate({scrollTop: $('.ab').offset().top}, 'slow');
+
+    });
+
+
+});
+
