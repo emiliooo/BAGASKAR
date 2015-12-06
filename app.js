bff4e0dbfc5b5603cf812deb6b935cf1b5c83993
emiliooo <emilburaczynski@gmail.com>
2015-12-06 13:06:45 +0100

    sticky menu Done!

--- app.js
+++ app.js
@@ -4,37 +4,60 @@
 $(document).ready(function() {
     $(".topOne").click(function(){
 
-        $('html, body').animate({scrollTop: $('.ab').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.ab').offset().top},2000);
 
     });
     $(".topTwo").click(function(){
 
-        $('html, body').animate({scrollTop: $('.our1').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.our1').offset().top}, 2000);
 
     });
 
     $(".toptree").click(function(){
 
-        $('html, body').animate({scrollTop: $('.numb').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.numb').offset().top},2000);
 
     });
 
     $(".topfiv").click(function(){
 
-        $('html, body').animate({scrollTop: $('.portf').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.portf').offset().top}, 2000);
 
     });
 
     $(".topsix").click(function(){
 
-        $('html, body').animate({scrollTop: $('.form').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.form').offset().top}, 2000);
 
     });
     $(".topse").click(function(){
 
-        $('html, body').animate({scrollTop: $('.form').offset().top}, 'slow');
+        $('html, body').animate({scrollTop: $('.form').offset().top}, 2000);
 
     });
+
+
+    //STICKY menu
+
+    var stickyNavTop=$('.sticked').offset().top;
+
+    var sticky=function(){
+        var scrollTop=$(window).scrollTop();
+
+        if(scrollTop >stickyNavTop){
+        $('.sticked').addClass("stick")
+        }else{
+        $('.sticked').removeClass("stick")
+        }
+    }
+
+    sticky()
+
+    $(window).scroll(function () {
+        sticky()
+    })
+
+
 
 
 
